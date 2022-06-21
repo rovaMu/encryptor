0f2d75dc17874494f440a4a980d994057b3d9bb4
Roveliu Munteanu <81239632+rovaMu@users.noreply.github.com>
2022-06-21 20:06:57 +0300

    Update encryptor.py

--- encryption/encryptor/encryptor.py
+++ encryption/encryptor/encryptor.py
@@ -17,7 +17,7 @@ f = Fernet(key)
 with open(filename, 'rb')as original_file:
     original = original_file.read()
 encrypted = f.encrypt(original)
-with open('file_encrypted.nigus', 'wb')as encrypted_file:
+with open('file_encrypted.rova', 'wb')as encrypted_file:
     encrypted_file.write(encrypted)
 f_name, f_ext = os.path.splitext(filename)
 with open('extension.txt', 'w') as f:
